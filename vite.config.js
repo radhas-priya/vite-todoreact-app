0b1ba6cccf144c6bc2fb6263faf68693c2147128
Priya Arya <107670700+radhas-priya@users.noreply.github.com>
2024-04-05 16:35:21 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: import.meta.env.PROD ? '/todo-react-app' : '/',
+  build: {
+    outDir: 'dist', 
+    // Specify the output directory (relative to project root)
+  },
 })
 // server:{port:3000}
