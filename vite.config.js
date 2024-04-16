abf36d31f46283928c6296d1968cc97c4d91ebdc
Priya Arya <107670700+radhas-priya@users.noreply.github.com>
2024-04-16 20:41:32 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-   base: import.meta.env.PROD ? '/todo-react-app' : '/',
-  build: {
-    outDir: 'dist', 
-    // Specify the output directory (relative to project root)
-  },
 })
 // server:{port:3000}
